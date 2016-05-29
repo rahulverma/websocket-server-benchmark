728205ac9deb12565cee438bfd7760094906b5f7
Rahul Verma <rahul.verma@contify.com>
2016-05-29 10:22:37 +0530

    Refactor and simplify to reduce memory usage Try uvloop

--- wsbench.py
+++ wsbench.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import asyncio
+import uvloop
+import websockets
+import argparse
 from random import randint
 from multiprocessing import Pool
+import os
 
-import websockets
-
-cpu_count = 4
+process_count = 4
 
 
 def get_address():
@@ -18,6 +20,7 @@ def get_address():
 async def connect(generator):
     async with websockets.connect('ws://localhost:9001', local_addr=next(generator)) as websocket:
         while True:
+            # print('Local Address: {}'.format(websocket.local_address))
             await websocket.send('*')
 
             response = await websocket.recv()
@@ -27,22 +30,46 @@ async def connect(generator):
 
             await asyncio.sleep(10 + randint(0, 10))
 
+
 async def capture_exception(generator):
     while True:
         try:
             await connect(generator)
         except Exception as e:
             print('Error: {}'.format(e))
-            await asyncio.sleep(1 + randint(100, 1000))
+            return
 
-async def multiple_connect(size):
+
+def multiple_connect(size):
     generator = get_address()
-    await asyncio.wait([capture_exception(generator) for _ in range(size)])
+    loop = asyncio.get_event_loop()
+    for _ in range(size):
+        loop.create_task(capture_exception(generator))
 
 
 def start(connection_count):
-    asyncio.get_event_loop().run_until_complete(multiple_connect(connection_count))
+    loop = uvloop.new_event_loop()
+    asyncio.set_event_loop(loop)
+    multiple_connect(connection_count)
+    asyncio.get_event_loop().run_forever()
 
 
-with Pool(4) as p:
-    p.map(start, [100000]*cpu_count)
+parser = argparse.ArgumentParser(description='Websocket Benchmark.')
+parser.add_argument('-p', dest='parallel_processes', action='store',
+                    type=int, nargs='?', default=os.cpu_count(),
+                    help='Number of parallel processes')
+
+parser.add_argument('-c', dest='connection_count', action='store',
+                    type=int, nargs='?', default=100,
+                    help='Connections per process.')
+
+
+args = parser.parse_args()
+
+if args.parallel_processes == 1:
+    start(args.connection_count)
+else:
+    process_count = args.parallel_processes
+    with Pool(process_count) as p:
+        p.map(start, [args.connection_count]*process_count)
+
